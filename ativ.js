a88b3f7f6e2431033998118ee5db9af1fb12dd6e
Igor Rafael <igor_rafa_lima@hotmail.com>
2022-01-31 21:35:30 -0300

    Adaptado

--- ativ.js
+++ ativ.js
@@ -3,14 +3,12 @@ const catBtn = document.getElementById('change-cat');
 const catImg = document.getElementById('cat');
 
 const getCats = async() => {
- try{
-  const data = await fetch(BASE_URL);
+  const data = await fetch(BASE_URL)
+    .then(res=> res.json())
+    .catch(e => console.log(e));
   const json = await data.json();
 
-  return json.webpurl
- }catch (e){
-   console.log(e.message)
- }
+  return data.webpurl
 };
 
 const loadImg = async() => {
